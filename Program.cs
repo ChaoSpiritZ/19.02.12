5da9cba716faa98fcdb243966326a625505e4d23
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-02-19 19:42:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insert 2 numbers");
+
+
+            int x;
+            int y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
